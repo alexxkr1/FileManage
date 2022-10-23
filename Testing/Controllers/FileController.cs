0fd73a838c5b5b2a4076c612b859965ab7765da3
Alexxkr1 <Alexandroskrenstrauch@gmail.com>
2022-10-24 00:00:22 +0300

    first commit

---
+++ Testing/Controllers/FileController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace Testing.Controllers
+{
+    public class FileController : Controller
+    {
+        private IHostingEnvironment _env;
+        // GET: FilesController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public FileController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        public IActionResult SingleFile(IFormFile file)
+        {
+
+            var dir = _env.ContentRootPath;
+
+            using(var fileStream = new FileStream(Path.Combine(dir, "file.png"), FileMode.Create, FileAccess.Write))
+            {
+                file.CopyTo(fileStream);
+            }
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
+  
---
+++ Testing/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using Testing.Areas.Identity.Data;
+using Testing.Models;
+
+namespace Testing.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.User.ToListAsync());
+        }
+
+        // GET: TooliVahendus/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tooliVahendus = await _context.User.FindAsync(id);
+            if (tooliVahendus == null)
+            {
+                return NotFound();
+            }
+            return View(tooliVahendus);
+        }
+
+        // POST: TooliVahendus/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("FirstName, LastName")] ApplicationUser tooliVahendus)
+        {
+    
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tooliVahendus);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                  
+                   
+                        throw;
+                   
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tooliVahendus);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
